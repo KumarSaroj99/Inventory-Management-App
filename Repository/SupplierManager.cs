cdbe9a233ffd792b87d34440af3a9ed8895555d2
KumarSaroj99 <sarojkumar.panda@aurionpro.com>
2024-08-15 22:30:47 +0530

    Update SupplierManager.cs

--- Repository/SupplierManager.cs
+++ Repository/SupplierManager.cs
@@ -11,104 +11,104 @@ namespace InventoryManagementSystem.Repository
 {
     internal class SupplierManager
     {
-        List<Supplier> suppliers = new List<Supplier>();
+    List<Supplier> suppliers = new List<Supplier>();
 
-        private readonly InventoryContext _context;
+    private readonly InventoryContext _context;
 
-        public SupplierManager(InventoryContext context)
+    public SupplierManager(InventoryContext context)
+    {
+        _context = context;
+    }
+
+
+    public void AddSupplier(string name , string description , int inventoryId)
+    {
+        if (CheckSupplierNameExists(name))
         {
-            _context = context;
+            throw new DuplicateItemException("Supplier name already exists!!! Please use a different name.");
         }
+        Supplier supplier = new Supplier(name,description , inventoryId);
+        _context.Suppliers.Add(supplier);
+        _context.SaveChanges();
+    }
 
-
-        public void AddSupplier(string name , string description , int inventoryId)
+    public void DeleteSupplier(int id)
+    {
+        
+        var supplier = FindSupplierById(id);
+        if (supplier == null)
         {
-            if (IsSupplierNameExists(name))
-            {
-                throw new DuplicateItemException("Supplier name already exists. Please use a different name.");
-            }
-            Supplier supplier = new Supplier(name,description , inventoryId);
-            _context.Suppliers.Add(supplier);
-            _context.SaveChanges();
+            throw new ItemNotFoundException("Supplier not found.");      
         }
-
-        public void DeleteSupplier(int id)
-        {
-            
-            var supplier = FindSupplierById(id);
-            if (supplier == null)
-            {
-                throw new ItemNotFoundException("Supplier not found.");      
-            }
-            _context.Suppliers.Remove(supplier);
-            _context.SaveChanges();
-
-        }
-
-        public Supplier FindSupplierById(int id)
-        {
-            try
-            {
-
-                var findSupplier = _context.Suppliers.Where(x => x.SupplierId == id).FirstOrDefault();
-                if (findSupplier == null)
-                {
-                    throw new ItemNotFoundException("Supplier not found");
-                }
-                return findSupplier;
-            }
-            catch 
-            {
-                throw new InvalidIdException("Invalid Id");
-            }
-        }
-
-        public List<Supplier> GetAllSuppliers()
-        {
-            return _context.Suppliers.ToList();
-        }
-
-
-        public bool IsSupplierNameExists(string name)
-        {
-            return _context.Suppliers.Any(p => p.SupplierName == name);
-        }
-
-        public void UpdateSupplier(int id, int fieldChoice, string newValue)
-        {
-
-            var supplier = FindSupplierById(id);
-            if (supplier == null)
-            {
-                throw new ItemNotFoundException("Supplier not found.");
-            }
-            switch (fieldChoice)
-            {
-                case 1:
-                    if (IsSupplierNameExists(newValue))
-                    {
-                        throw new DuplicateItemException("Supplier name already exists. Please use a different name.");
-                    }
-                    supplier.SupplierName = newValue;
-                    break;
-                case 2:
-                    supplier.ContactInformation = newValue;
-                    break;
-              
-                default:
-                    throw new Exception("Invalid field choice.");
-            }
-
-            _context.Suppliers.Update(supplier);
-            _context.SaveChanges();
-        }
-
-        public Supplier GetSupplierDetails(int id)
-        {
-            
-            return _context.Suppliers.FirstOrDefault(p => p.SupplierId == id);
-
-        }
+        _context.Suppliers.Remove(supplier);
+        _context.SaveChanges();
 
     }
+
+    public Supplier FindSupplierById(int id)
+    {
+        try
+        {
+
+            var findSupplier = _context.Suppliers.Where(item => item.SupplierId == id).FirstOrDefault();
+            if (findSupplier == null)
+            {
+                throw new ItemNotFoundException("Supplier not found");
+            }
+            return findSupplier;
+        }
+        catch 
+        {
+            throw new InvalidIdException("Invalid Id");
+        }
+    }
+
+    public List<Supplier> GetAllSuppliers()
+    {
+        return _context.Suppliers.ToList();
+    }
+
+
+    public bool CheckSupplierNameExists(string name)
+    {
+        return _context.Suppliers.Any(item => item.SupplierName == name);
+    }
+
+    public void UpdateSupplier(int id, int fieldChoice, string newValue)
+    {
+
+        var supplier = FindSupplierById(id);
+        if (supplier == null)
+        {
+            throw new ItemNotFoundException("Supplier not found.");
+        }
+        switch (fieldChoice)
+        {
+            case 1:
+                if (CheckSupplierNameExists(newValue))
+                {
+                    throw new DuplicateItemException("Supplier name already exists. Please use a different name.");
+                }
+                supplier.SupplierName = newValue;
+                break;
+            case 2:
+                supplier.ContactInformation = newValue;
+                break;
+          
+            default:
+                throw new Exception("Invalid field choice.");
+        }
+
+        _context.Suppliers.Update(supplier);
+        _context.SaveChanges();
+    }
+
+    public Supplier GetSupplierDetails(int id)
+    {
+        
+        return _context.Suppliers.FirstOrDefault(item => item.SupplierId == id);
+
+    }
+
+   }
 }
