845257478918b800bf8e76cc7a7ee2ec89adf1ce
KumarSaroj99 <sarojkumar.panda@aurionpro.com>
2024-08-15 22:28:10 +0530

    Update ProductManager.cs

--- Repository/ProductManager.cs
+++ Repository/ProductManager.cs
@@ -11,151 +11,150 @@ namespace InventoryManagementSystem.Repository
 {
     internal class ProductManager
     {
-        List<Supplier> products = new List<Supplier>();
-        List<Transaction> transactions = new List<Transaction>();
+    List<Supplier> products = new List<Supplier>();
+    List<Transaction> transactions = new List<Transaction>();
 
-        private readonly InventoryContext _context;
+    private readonly InventoryContext _context;
 
-        public ProductManager(InventoryContext context)
+    public ProductManager(InventoryContext context)
+    {
+        _context = context;
+    }
+
+    public void AddProduct(string name , string description , int quantity , double price , int inventoryId)
+    {
+        if (IsProductNameExists(name))
         {
-            _context = context;
+            throw new DuplicateItemException("Product name already exists!!! Please use a different name.");
         }
 
-        public void AddProduct(string name , string desc , int quantity , double price , int inventoryId)
+        Product product = Product.CreateProduct(name , description, quantity , price , inventoryId);
+        _context.Products.Add(product);
+        _context.SaveChanges();
+    }
+
+    public void DeleteProduct(int id)
+    {
+        var product = FindProductById(id);
+        if (product == null)
         {
-            if (IsProductNameExists(name))
+            throw new ItemNotFoundException("Product not found.");
+        }
+
+        _context.Products.Remove(product);
+        _context.SaveChanges();
+    }
+
+    public Product FindProductById(int id)
+    {
+        try
+        {
+           
+            var findProduct = _context.Products.Where(item => item.ProductId == id).FirstOrDefault();
+            if (findProduct == null)
             {
-                throw new DuplicateItemException("Product name already exists. Please use a different name.");
+                throw new ItemNotFoundException("Product not found");
             }
-
-            Product product = Product.CreateProduct(name , desc , quantity , price , inventoryId);
-            _context.Products.Add(product);
-            _context.SaveChanges();
+            return findProduct;
         }
-
-        public void DeleteProduct(int id)
+        catch
         {
-            var product = FindProductById(id);
-            if (product == null)
-            {
-                throw new ItemNotFoundException("Product not found.");
-            }
-
-            _context.Products.Remove(product);
-            _context.SaveChanges();
+            throw new InvalidIdException("Invalid Id");
         }
+    }
 
-        public Product FindProductById(int id)
+    public bool IsProductNameExists(string name)
+    {
+        return _context.Products.Any(item => item.ProductName == name);
+    }
+    public List<Product> GetProducts() { 
+        return _context.Products.ToList();
+    }
+
+    public void UpdateProduct(int id, int fieldChoice, string newValue)
+    {
+
+        var product = FindProductById(id);
+        if (product == null)
         {
-            try
-            {
-               
-                var findProduct = _context.Products.Where(x => x.ProductId == id).FirstOrDefault();
-                if (findProduct == null)
+            throw new ItemNotFoundException("Product not found.");
+        }
+        switch (fieldChoice)
+        {
+            case 1:
+                if (IsProductNameExists(newValue))
                 {
-                    throw new ItemNotFoundException("Product not found");
+                    throw new DuplicateItemException("Product name already exists!!! Please use a different name.");
                 }
-                return findProduct;
-            }
-            catch
-            {
-                throw new InvalidIdException("Invalid Id");
-            }
+                product.ProductName = newValue;
+                break;
+            case 2:
+                product.ProductDescription = newValue;
+                break;
+            case 3:
+                if (double.TryParse(newValue, out double price))
+                {
+                    product.ProductPrice = price;
+                }
+                else
+                {
+                    throw new FormatException("Invalid price value.");
+                }
+                break;
+            default:
+                throw new Exception("Invalid field choice.");
         }
 
-        public List<Product> GetProducts() { 
-            return _context.Products.ToList();
-        }
+        _context.Products.Update(product);
+        _context.SaveChanges();
+    }
 
-        public bool IsProductNameExists(string name)
+    public void AddStock(int id, int newValue)
+    {
+        var product = FindProductById(id);
+        if (product == null)
         {
-            return _context.Products.Any(p => p.ProductName == name);
+            throw new ItemNotFoundException("Product not found.");
         }
-
-        public void UpdateProduct(int id, int fieldChoice, string newValue)
-        {
-
-            var product = FindProductById(id);
-            if (product == null)
-            {
-                throw new ItemNotFoundException("Product not found.");
-            }
-            switch (fieldChoice)
-            {
-                case 1:
-                    if (IsProductNameExists(newValue))
-                    {
-                        throw new DuplicateItemException("Product name already exists. Please use a different name.");
-                    }
-                    product.ProductName = newValue;
-                    break;
-                case 2:
-                    product.ProductDescription = newValue;
-                    break;
-                case 3:
-                    if (double.TryParse(newValue, out double price))
-                    {
-                        product.ProductPrice = price;
-                    }
-                    else
-                    {
-                        throw new FormatException("Invalid price value.");
-                    }
-                    break;
-                default:
-                    throw new Exception("Invalid field choice.");
-            }
-
-            _context.Products.Update(product);
-            _context.SaveChanges();
-        }
-
-        public void AddStock(int id, int newValue)
-        {
-            var product = FindProductById(id);
-            if (product == null)
-            {
-                throw new ItemNotFoundException("Product not found.");
-            }
-            product.ProductQuantity += newValue;
-            _context.Products.Update(product);
-            
-            Transaction transaction = Transaction.CreateTransaction(id, "Stock Added", newValue ,(int)product.InventoryId);
-            _context.Transactions.Add(transaction);
-            _context.SaveChanges();
-
-        }
-
-        public void RemoveStock(int id, int newValue)
-        {
-            var product = FindProductById(id);
-            if (product == null)
-            {
-                throw new ItemNotFoundException("Product not found.");
-            }
-            product.ProductQuantity -= newValue;
-            _context.Products.Update(product);
-            Transaction transaction = Transaction.CreateTransaction(id, "Stock Removed", newValue , (int)product.InventoryId);
-            _context.Transactions.Add(transaction);
-            _context.SaveChanges();
-        }
-
-        public Product GetProductDetails(int id)
-        {
-            return _context.Products.FirstOrDefault(p => p.ProductId == id);
-
-        }
-
-        public List<Product> GetAllProducts()
-        {
-            return _context.Products.ToList();
-        }
-
-        public List<Transaction> GetTransactions()
-        {
-            return _context.Transactions.ToList();
-        }
-
+        product.ProductQuantity += newValue;
+        _context.Products.Update(product);
+        
+        Transaction transaction = Transaction.CreateTransaction(id, "Stock Added", newValue ,(int)product.InventoryId);
+        _context.Transactions.Add(transaction);
+        _context.SaveChanges();
 
     }
+
+    public void RemoveStock(int id, int newValue)
+    {
+        var product = FindProductById(id);
+        if (product == null)
+        {
+            throw new ItemNotFoundException("Product not found.");
+        }
+        product.ProductQuantity -= newValue;
+        _context.Products.Update(product);
+        Transaction transaction = Transaction.CreateTransaction(id, "Stock Removed", newValue , (int)product.InventoryId);
+        _context.Transactions.Add(transaction);
+        _context.SaveChanges();
+    }
+
+    public Product GetProductDetails(int id)
+    {
+        return _context.Products.FirstOrDefault(item => item.ProductId == id);
+
+    }
+
+    public List<Product> GetAllProducts()
+    {
+        return _context.Products.ToList();
+    }
+
+    public List<Transaction> GetTransactions()
+    {
+        return _context.Transactions.ToList();
+    }
+
+
+  }
 }
