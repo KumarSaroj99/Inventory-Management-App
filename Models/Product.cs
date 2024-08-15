c812a65924cc1afbbec6e9142dde1718b02b86b9
KumarSaroj99 <sarojkumar.panda@aurionpro.com>
2024-08-15 22:02:28 +0530

    Update Product.cs

--- Models/Product.cs
+++ Models/Product.cs
@@ -9,42 +9,44 @@ using System.Threading.Tasks;
 namespace InventoryManagementSystem.Models
 {
     internal class Product
+{
+    [Key]
+    public int ProductId {  get; set; }
+    public string ProductName { get; set; }
+
+    public string ProductDescription { get; set; }
+
+    public int ProductQuantity { get; set; }
+    
+    public double ProductPrice { get; set; }
+
+    [ForeignKey("Inventory")]
+
+    public int? InventoryId { get; set; }
+    public Inventory inventory { get; set; }
+    public Product() { }
+    public Product(string name, string desc, int quantity, double price , int inventoryId)
     {
-        [Key]
-public int ProductId {  get; set; }
-public string ProductName { get; set; }
-
-public string ProductDescription { get; set; }
-
-public int ProductQuantity { get; set; }
-
-public double ProductPrice { get; set; }
-
-[ForeignKey("Inventory")]
-
-public int? InventoryId { get; set; }
-public Inventory inventory { get; set; }
-public Product() { }
-public Product(string name, string desc, int quantity, double price , int inventoryId)
-{
-    ProductName = name;
-    ProductDescription = desc;
-    ProductQuantity = quantity;
-    ProductPrice = price;
-    InventoryId = inventoryId;
-}
-public static Product CreateProduct(string name, string desc , int quantity , double price , int inventoryId)
-{
-    return new Product(name,desc , quantity , price , inventoryId);
-}
-public override string ToString()
-{
-    return $"\n=====Product Id : {ProductId}======\n" +
-        $"Product Name : {ProductName}\n" +
-        $"Product Desc : {ProductDescription}\n" +
-        $"Product Quantiy : {ProductQuantity}\n" +
-        $"Product Price : {ProductPrice}\n";
-
-        }
+        ProductName = name;
+        ProductDescription = desc;
+        ProductQuantity = quantity;
+        ProductPrice = price;
+        InventoryId = inventoryId;
     }
+    public static Product CreateProduct(string name, string description , int quantity , double price , int inventoryId)
+    {
+        return new Product(
+            name, description, quantity , price , inventoryId);
+    }
+    public override string ToString()
+    {
+        return $"\n=====Product Id : {ProductId}======\n" +
+            $"Product Name : {ProductName}\n" +
+            $"Product Desc : {ProductDescription}\n" +
+            $"Product Quantiy : {ProductQuantity}\n" +
+            $"Product Price : {ProductPrice}\n";
+
+    }
+
+}
 }
