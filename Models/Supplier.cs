bfb995666d1b26bd1261f011318c99ad31a24b67
KumarSaroj99 <sarojkumar.panda@aurionpro.com>
2024-08-15 22:12:34 +0530

    Update Supplier.cs

--- Models/Supplier.cs
+++ Models/Supplier.cs
@@ -10,36 +10,36 @@ namespace InventoryManagementSystem.Models
 {
     internal class Supplier
     {
-        [Key]
-        public int SupplierId { get; set; }
-        public string SupplierName { get; set; }
-        public string ContactInformation {  get; set; }
+    [Key]
+    public int SupplierId { get; set; }
+    public string SupplierName { get; set; }
+    public string ContactInformation {  get; set; }
 
-        [ForeignKey("Inventory")]
-        public int? InventoryId { get; set; }
-     
-        public Inventory inventory { get; set; }
+    [ForeignKey("Inventory")]
+    public int? InventoryId { get; set; }
+ 
+    public Inventory inventory { get; set; }
 
-        public Supplier()
-        {
-            
-        }
-
-        public Supplier(string supplierName , string contactInformation , int inventoryId)
-        {
-            SupplierName = supplierName;
-            ContactInformation = contactInformation;
-            InventoryId = inventoryId;
-        }
-        public static Supplier CreateSupplier(string supplierName, string contactInformation , int inventoryId)
-        {
-            return new Supplier(supplierName,contactInformation , inventoryId);
-        }
-        public override string ToString()
-        {
-            return $"\nSupplierId : {SupplierId}\n" +
-                $"Supplier Name : {SupplierName}\n" +
-                $"Supplier Contact : {ContactInformation}\n";
-        }
+    public Supplier()
+    {
+        
     }
+
+    public Supplier(string supplierName , string contactInfo , int inventoryId)
+    {
+        SupplierName = supplierName;
+        ContactInformation = contactInfo;
+        InventoryId = inventoryId;
+    }
+    public static Supplier CreateSupplier(string supplierName, string contactInfo , int inventoryId)
+    {
+        return new Supplier(supplierName,contactInfo , inventoryId);
+    }
+    public override string ToString()
+    {
+        return $"\n=====SupplierId : {SupplierId}======\n" +
+            $"Supplier Name : {SupplierName}\n" +
+            $"Supplier Contact : {ContactInformation}\n";
+    }
+  }
 }
