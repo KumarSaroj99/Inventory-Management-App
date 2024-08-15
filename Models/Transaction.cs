dbcf56996b16882f30c325a3c1a1d2a1a1a890bb
KumarSaroj99 <sarojkumar.panda@aurionpro.com>
2024-08-15 22:10:33 +0530

    Update Transaction.cs

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -9,44 +9,44 @@ using System.Threading.Tasks;
 namespace InventoryManagementSystem.Models
 {
     internal class Transaction
+ {
+    [Key]
+    public int TransactionId { get; set; }
+    public int ProductId { get; set; }
+    public string TransactionType { get; set; }
+    public int TransactionQuantity { get; set; }
+    public DateTime TransactionDate { get; set; }
+
+    [ForeignKey("Inventory")]
+    public int? InventoryId { get; set; }
+    public Inventory inventory { get; set; }
+
+    public Transaction()
     {
-        [Key]
-        public int TransactionId { get; set; }
-        public int ProductId { get; set; }
-        public string TransactionType { get; set; }
-        public int TransactionQuantity { get; set; }
-        public DateTime TransactionDate { get; set; }
-
-        [ForeignKey("Inventory")]
-        public int? InventoryId { get; set; }
-        public Inventory inventory { get; set; }
-
-        public Transaction()
-        {
-            
-        }
-
-        public Transaction(int productid , string type , int quantity , DateTime date , int inventoryId)
-        {
-            ProductId = productid;
-            TransactionType = type;
-            TransactionQuantity = quantity;
-            TransactionDate = date;
-            InventoryId = inventoryId;
-
-        }
-
-        public static Transaction CreateTransaction(int productId, string type , int quantity , int inventoryId)
-        {
-            return new Transaction(productId , type , quantity , DateTime.Now , inventoryId);
-        }
-
-        public override string ToString() { 
-            return $"\nTransaction Id : {TransactionId}\n" +
-                $"Product Id : {ProductId}\n" +
-                $"Transaction Type : {TransactionType} \n" +
-                $"Transaction Quantity : {TransactionQuantity}\n" +
-                $"Transaction Date : {TransactionDate}\n";
-        }
+        
     }
+
+    public Transaction(int productid , string type , int quantity , DateTime date , int inventoryId)
+    {
+        ProductId = productid;
+        TransactionType = type;
+        TransactionQuantity = quantity;
+        TransactionDate = date;
+        InventoryId = inventoryId;
+
+    }
+
+    public static Transaction CreateTransaction(int productId, string type , int quantity , int inventoryId)
+    {
+        return new Transaction(productId , type , quantity , DateTime.Now , inventoryId);
+    }
+
+    public override string ToString() { 
+        return $"\n=====Transaction Id : {TransactionId}======\n" +
+            $"Product Id : {ProductId}\n" +
+            $"Transaction Type : {TransactionType} \n" +
+            $"Transaction Quantity : {TransactionQuantity}\n" +
+            $"Transaction Date : {TransactionDate}\n";
+    }
+  }
 }
