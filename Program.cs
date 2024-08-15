ad684b78992a99d14106e761e62c6c3ce8da4907
KumarSaroj99 <sarojkumar.panda@aurionpro.com>
2024-08-15 16:20:31 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,32 +10,6 @@ namespace InventoryManagementSystem
     {
         static void Main(string[] args)
         {
-            //ProductManager productManger = new ProductManager(new Services.InventoryContext());
-            //SupplierManager supplierManger = new SupplierManager(new Services.InventoryContext());
-
-            //Product product = new Product
-            //{
-            //    ProductId = 2,
-            //    ProductName = "Bikes",
-            //    ProductDescription = "Nice Bikes",
-            //    ProductQuantity = 10,
-            //    ProductPrice = 2100
-            //};
-            //productManger.UpdateProduct(product);
-
-            //Supplier supplier = new Supplier
-            //{
-            // SupplierName= "Sam",
-            // ContactInformation="1234567890"
-
-            //};
-            //supplierManger.AddSupplier(supplier);
-            //productManger.DeleteProduct(1);
-            //var products = productManger.GetProducts();
-            //products.ForEach(p => Console.WriteLine(p));
-
-            //Console.WriteLine(productManger.FindProductById(2));
-
 
             InventoryStore.DisplayMenu();
 
